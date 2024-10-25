1511a094ae69b07a4831b324c953c7e626eee7be
Bryan-Dos-Santos-Ribeiro <bryan.dossantosribeiro@he-arc.ch>
2024-10-25 11:02:40 +0200

    view ingredients

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('content')
+    <h1>CulinArc</h1>
+    <p class="lead">Hello World</p>
+@endsection
---
+++ resources/views/ingredients/edit.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.app')
+
+@section('content')
+
+<div class="row">
+    <div class="col-12">
+        <a class="btn btn-primary" href="/ingredients"> Retour</a>
+    </div>
+</div>
+
+<form action="/ingredients/{{$ingredient->id}}" method="POST">
+    @csrf
+    @method('PUT')
+
+    <div class="card col-12 col-lg-6 offset-0 offset-lg-3">
+        <div class="card-header">
+            Modifier un ingrédient
+        </div>
+        <div class="card-body">
+            <div class="form-row">
+                <div class="form-group col-12">
+                    <label for="inputName">Nom</label>
+                    <input type="text" name="name" value="{{$ingredient->name}}" class="form-control" id="inputName">
+                </div>
+                <div class="row mt-3">
+                    <div class="form-group col-6">
+                        <label for="inputUnit">Unité</label>
+                        <select name="unit" class="form-control" id="inputUnit">
+                            @foreach(App\Models\Ingredient::UNITS as $id => $unit)
+                                <option value="{{ $id }}" {{ $ingredient->unit == $unit ? 'selected' : '' }}>
+                                    {{ $unit }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary mt-3">Modifier</button>
+            </div>
+        </div>
+    </div>
+
+</form>
+@endsection
