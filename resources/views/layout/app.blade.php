712d376c9968c3c58daa87c245253f176e2d5fe5
Bryan-Dos-Santos-Ribeiro <bryan.dossantosribeiro@he-arc.ch>
2024-11-15 13:58:14 +0100

    Home page with recipes in cards added

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,6 +11,10 @@
 
     <!-- Bootstrap Icons CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
