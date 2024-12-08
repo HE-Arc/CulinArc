283d183e9a5a7c4f8283d05711e9cd186132770f
Magnin Nicolas <nicolas.magnin@he-arc.ch>
2024-12-08 19:15:15 +0100

    Add login and registration

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,30 @@
+
+@extends('layout.app')
+
+@section('content')
+<h2>Login</h2>
+
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+    <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+    </div>
+    <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+</form>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,29 @@ use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\IngredientController;
 use App\Http\Controllers\UserController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [RecipeController::class, 'index']);
+Route::get('home', function () {
+    return redirect('/');
+})->name('home');
 
 Route::resource('ingredients', IngredientController::class);
 Route::resource('recipes', RecipeController::class);
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', [UserController::class, 'index'])->name('login');
+    Route::post('login', [UserController::class, 'login']);
+    Route::get('register', [UserController::class, 'create'])->name('register');
+    Route::post('register', [UserController::class, 'store']);
+});
+
+
+Route::post('logout', [UserController::class, 'logout'])->name('logout');
