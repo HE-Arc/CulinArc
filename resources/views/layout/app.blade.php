5a3210c92c9b8fb69295cdfa5c1a5715250e9364
NicolasMagnin <129372845+NicolasMagnin@users.noreply.github.com>
2024-12-18 10:18:20 +0100

    Add logo in nav (#54)

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -19,7 +19,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('recipes.index') }}">Accueil</a>
+            <a class="navbar-brand" href="{{ route('recipes.index') }}"><img src="{{ asset('images/culinarc_transp.png') }}" alt="Logo CulinArc" class="img-fluid logo"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -49,11 +49,11 @@
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="{{ route('register') }}">Créer un compte</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="{{ route('about') }}">À propos</a>
-                  </li>
                 @endif
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('about') }}">À propos</a>
+                </li>
               </ul>
             </div>
         </div>
